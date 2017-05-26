3b6b5ba11b8c0cf10b2ab4c31afbc977058e93d7
Kirill Vorobiov <mishkacod@gmail.com>
2017-05-26 17:47:58 +0300

    update template, add gulp-sass-lint, add favicon

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 'use strict';
 
 /*
- * Optimized Template Copyright © 2017, Kirill Vorobyov mishkacod@gmail.com
- * ISC Licensed
+ * Gulp Optimized Template Copyright © 2017, Kirill Vorobyov mishkacod@gmail.com
+ * MIT Licensed
  */
 
 /**
@@ -20,15 +20,31 @@ const combineMq = require('gulp-combine-mq');
 const concat = require('gulp-concat');
 const csscomb = require('gulp-csscomb');
 const imagemin = require('gulp-imagemin');
+const inject = require('gulp-inject');
 const notify = require("gulp-notify");
 const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
+const series = require('stream-series');
 const size = require('gulp-size');
 const sass = require('gulp-sass');
+const sassLint = require('gulp-sass-lint');
 const sourcemaps = require('gulp-sourcemaps');
 const spritesmith = require('gulp.spritesmith');
 const uglify = require('gulp-uglify');
 
+/**
+ * Path
+ * -----------------------------------------------------------------------------
+ */
+
+const dirs = {
+	buildPath: './build/',
+	srcPath: './src/'
+}
+
+const sprite = {
+	src: './src/images/sprites/*.*'
+}
 
 /**
  * Local dev server with live reload
@@ -36,12 +52,12 @@ const uglify = require('gulp-uglify');
  */
 
 gulp.task('server', () => {
-	// Create and initialize local server
-	browserSync.init({
+
+	return browserSync.init({
 		server: {
-			baseDir: 'src'
+			baseDir: dirs.srcPath
 		},
-		logPrefix: "optimizedtemplate",
+		logPrefix: "gulp-optimized-template",
 		port: 8080,
 		startPath: 'index.html',
 		notify: false
@@ -56,7 +72,7 @@ gulp.task('server', () => {
 
 gulp.task('styles', () => {
 
-	return gulp.src('src/sass/**/*.sass')
+	return gulp.src(dirs.srcPath + '/sass/**/*.sass')
 		.pipe(plumber({
 			errorHandler: notify.onError(function (err) {
 				return {
@@ -67,7 +83,11 @@ gulp.task('styles', () => {
 		}))
 		.pipe(sourcemaps.init())
 		.pipe(sass())
-		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8']))
+		.pipe(autoprefixer([
+			'last 15 versions',
+			'> 1%',
+			'ie 8'
+		]))
 		.pipe(csscomb())
 		.pipe(cleanCSS())
 		.pipe(rename({
@@ -76,14 +96,33 @@ gulp.task('styles', () => {
 		}))
 		.pipe(sourcemaps.write('/'))
 		.pipe(size({
-			title: 'Размер',
+			title: 'Size',
 			showFiles: true,
 			showTotal: false,
 		}))
-		.pipe(gulp.dest('src/css'))
+		.pipe(gulp.dest(dirs.srcPath + '/css'))
 		.pipe(browserSync.reload({
 			stream: true
 		}));
+
+});
+
+/**
+ * Lint Sass
+ * -----------------------------------------------------------------------------
+ */
+
+gulp.task('sass-lint', () => {
+
+	return gulp.src(dirs.srcPath + '/sass/**/*.sass')
+		.pipe(sassLint({
+			options: {
+				'config-file': '.sass-lint.yaml'
+			}
+		}))
+		.pipe(sassLint.format())
+		.pipe(sassLint.failOnError());
+
 });
 
 /**
@@ -92,12 +131,35 @@ gulp.task('styles', () => {
  */
 
 gulp.task('scripts', () => {
+
 	return gulp.src([
 		'bower_components/jquery/dist/jquery.min.js'
 	])
 		.pipe(concat('libs.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('src/js'));
+		.pipe(size({
+			title: 'Size',
+			showFiles: true,
+			showTotal: false,
+		}))
+		.pipe(gulp.dest(dirs.srcPath + '/js'));
+
+});
+
+/**
+ * Injects styles and scripts
+ * -----------------------------------------------------------------------------
+ */
+gulp.task('injects', () => {
+
+	const injectCss = gulp.src(dirs.srcPath + '/css/*.css', { read: false });
+	const injectLibsJs = gulp.src(dirs.srcPath + '/js/libs.min.js', { read: false });
+	const injectCommonJs = gulp.src(dirs.srcPath + '/js/common.js', { read: false });
+
+	return gulp.src(dirs.srcPath + '/**/*.html')
+		.pipe(inject(series(injectCss, injectLibsJs, injectCommonJs), { relative: true }))
+		.pipe(gulp.dest(dirs.srcPath));
+
 });
 
 /**
@@ -105,21 +167,22 @@ gulp.task('scripts', () => {
  * -----------------------------------------------------------------------------
  */
 
-gulp.task('images', () =>
+gulp.task('images', () => {
 
-	gulp.src('src/images/**/*')
+	return gulp.src(dirs.srcPath + '/images/**/*.*')
 		.pipe(imagemin({
 			interlaced: true,
 			progressive: true,
 			optimizationLevel: 5
 		}))
-		.pipe(gulp.dest('src/images/'))
+		.pipe(gulp.dest(dirs.srcPath + '/images/'))
 		.pipe(size({
-			title: 'Размер',
+			title: 'Size',
 			showFiles: true,
 			showTotal: false,
-		}))
-);
+		}));
+
+});
 
 /**
  * Build sprites
@@ -127,8 +190,9 @@ gulp.task('images', () =>
  */
 
 gulp.task('sprites', () => {
+
 	var spriteData =
-		gulp.src('./src/images/sprites/*.*')
+		gulp.src(sprite.src)
 			.pipe(spritesmith({
 				imgName: 'sprite.png',
 				cssName: '_sprites.sass',
@@ -136,8 +200,9 @@ gulp.task('sprites', () => {
 				algorithm: 'top-down',
 				padding: 5
 			}));
-	spriteData.img.pipe(gulp.dest('./src/images/'));
-	spriteData.css.pipe(gulp.dest('./src/sass/helpers/'));
+	spriteData.img.pipe(gulp.dest(dirs.srcPath + '/images/'));
+	spriteData.css.pipe(gulp.dest(dirs.srcPath + '/sass/helpers/'));
+
 });
 
 /**
@@ -145,8 +210,21 @@ gulp.task('sprites', () => {
  * -----------------------------------------------------------------------------
  */
 
-gulp.task('clean', () => {
+gulp.task('del', () => {
+
 	return del.sync('build');
+
+});
+
+/**
+ * Clear cashe
+ * -----------------------------------------------------------------------------
+ */
+
+gulp.task('clear', () => {
+
+	return cache.clearAll();
+
 });
 
 /**
@@ -154,22 +232,22 @@ gulp.task('clean', () => {
  * -----------------------------------------------------------------------------
  */
 
-gulp.task('build', ['clean', 'sprites', 'images', 'styles', 'scripts'], () => {
+gulp.task('build', ['del', 'sprites', 'styles', 'scripts', 'images', 'sass-lint'], () => {
 
-	const buildCss = gulp.src('src/css/**/*')
-		.pipe(gulp.dest('build/css'));
+	const buildCss = gulp.src(dirs.srcPath + '/css/**/*')
+		.pipe(gulp.dest(dirs.buildPath + '/css'));
 
-	const buildFonts = gulp.src('src/fonts/**/*')
-		.pipe(gulp.dest('build/fonts'));
+	const buildFonts = gulp.src(dirs.srcPath + '/fonts/**/*')
+		.pipe(gulp.dest(dirs.buildPath + '/fonts'));
 
-	const buildJs = gulp.src('src/js/**/*')
-		.pipe(gulp.dest('build/js'));
+	const buildJs = gulp.src(dirs.srcPath + '/js/**/*')
+		.pipe(gulp.dest(dirs.buildPath + '/js'));
 
-	const buildHtml = gulp.src('src/*.html')
-		.pipe(gulp.dest('build'));
+	const buildHtml = gulp.src(dirs.srcPath + '/**/*.html')
+		.pipe(gulp.dest(dirs.buildPath));
 
-	const buildImages = gulp.src('src/images/**/*')
-		.pipe(gulp.dest('build/images'));
+	const buildImages = gulp.src(dirs.srcPath + '/images/**/*')
+		.pipe(gulp.dest(dirs.buildPath + '/images'));
 
 });
 
@@ -178,12 +256,13 @@ gulp.task('build', ['clean', 'sprites', 'images', 'styles', 'scripts'], () => {
  * -----------------------------------------------------------------------------
  */
 
-gulp.task('watch', ['images', 'styles', 'scripts', 'server'], () => {
-	gulp.watch('src/*.html', browserSync.reload);
-	gulp.watch('src/sass/**/*.sass', ['styles']);
-	gulp.watch('src/libs/**/*.js', ['scripts']);
-	gulp.watch('src/js/**/*.js', browserSync.reload);
-	gulp.watch('src/images/**/*', browserSync.reload);
+gulp.task('watch', ['injects', 'sprites', 'styles', 'sass-lint', 'scripts', 'images', 'server'], () => {
+
+	gulp.watch(dirs.srcPath + '/**/*.html', browserSync.reload);
+	gulp.watch(dirs.srcPath + '/sass/**/*.sass', ['styles']);
+	gulp.watch(dirs.srcPath + '/js/**/*.js', browserSync.reload);
+	gulp.watch(dirs.srcPath + '/images/**/*', browserSync.reload);
+
 });
 
 /**
